c972f52f7d82f67eab95534c700f0478ff561bf8
Jared Shumway <jaredshumway94@gmail.com>
2016-01-29 22:50:02 -0800

    Terrain class outline

---
+++ terrain.lua
@@ -0,0 +1,28 @@
+local Terrain = {}
+
+function Terrain:new()
+    newObj = {
+        -- Types of tiles in the world
+        types = {},
+        -- The tiles in the world, organized width then height
+        layer = {}
+    }
+    self.__index = self
+    return setmetatable(newObj, self)
+end
+
+function Terrain:addType(name, repr, passable)
+    self.types.name = { repr = repr, passable = passable }
+end
+
+function Terrain:initLayer(width, height, terainType)
+    local ttype = self.types[terrainType]
+    for w in 0, width do
+        self.layer[w] = {}
+        for h in 0, height do
+            self.layer[w][h] = ttype
+        end
+    end
+end
+
+return Terrain
