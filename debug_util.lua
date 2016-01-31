2e77da50f1cf1851d172cb0f01c28a9f14f995c2
Jared Shumway <jaredshumway94@gmail.com>
2016-01-31 00:18:58 -0800

    Better DebugUtil.highlight_rect

--- debug_util.lua
+++ debug_util.lua
@@ -2,8 +2,17 @@ DebugUtil = {
     queue = {}
 }
 
-function DebugUtil:highlight_rect(r)
-    table.insert(self.queue, function ()
-        love.graphics.rectangle("line", r.x, r.y, r.w, r.h)
-    end)
+function DebugUtil:highlight_rect(rx, y, w, h)
+    -- Takes either a table { x = , y = , w = , h = } or x, y, w, and h as
+    -- arguments
+
+    if type(rx) == "table" then
+        table.insert(self.queue, function ()
+            love.graphics.rectangle("line", rx.x, rx.y, rx.w, rx.h)
+        end)
+    elseif rx ~= nil and y ~= nil and w ~= nil and h ~= nil then
+        table.insert(self.queue, function ()
+            love.graphics.rectangle("line", rx, y, w, h)
+        end)
+    end
 end
